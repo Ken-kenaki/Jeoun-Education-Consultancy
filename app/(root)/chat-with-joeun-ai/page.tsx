51046b9e1358385fea61168bdf7f5fb313e2af24
Ken-kenaki <kenofficial588@gmail.com>
2025-09-18 19:44:17 +0545

    chat-bot-removed-2

--- app/(root)/chat-with-joeun-ai/page.tsx
+++ app/(root)/chat-with-joeun-ai/page.tsx
@@ -1,11 +1,21 @@
-import Chatbot from '@/Components/chatbot'
+// import Chatbot from '@/Components/chatbot'
+// import React from 'react'
+
+// const page = () => {
+//   return (
+//     <div>
+//         <Chatbot />
+//     </div>
+//   )
+// }
+
+// export default page
+
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        <Chatbot />
-    </div>
+    <div>page</div>
   )
 }
 
